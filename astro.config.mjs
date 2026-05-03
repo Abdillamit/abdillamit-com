77bdc3f39c24cce16529232dd2c6e13fb46dd003
Abdillamit Mamytov <abdillamit08@gmail.com>
2026-05-03 20:23:12 +0600

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   site: 'https://abdillamit.com',
   integrations: [sitemap()],
+  adapter: vercel(),
   i18n: {
     defaultLocale: 'ru',
     locales: ['ru', 'en', 'ky'],
